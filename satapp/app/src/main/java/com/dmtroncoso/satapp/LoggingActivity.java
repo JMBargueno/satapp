6b9dd3aa2d1b21b861bf80b9c669239d2f6b5a3f
myrows <dmtroncoso@AULAS.TRIANA>
2020-02-26 14:06:24 +0100

    Login

--- satapp/app/src/main/java/com/dmtroncoso/satapp/LoggingActivity.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/LoggingActivity.java
@@ -16,11 +16,5 @@ public class LoggingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging);
 
-        mLayout = findViewById(R.id.constraintLayout);
-
-        animationDrawable = (AnimationDrawable) mLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(3000);
-        animationDrawable.setExitFadeDuration(3000);
-        animationDrawable.start();
     }
 }
