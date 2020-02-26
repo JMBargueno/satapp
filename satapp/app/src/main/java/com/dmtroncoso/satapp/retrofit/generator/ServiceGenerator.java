3ad8fee01751f6a08d658ee0a52858a18c75a1f5
myrows <milenio204@hotmail.com>
2020-02-26 17:50:48 +0100

    [UPD] Logging funcional

--- satapp/app/src/main/java/com/dmtroncoso/satapp/retrofit/generator/ServiceGenerator.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/retrofit/generator/ServiceGenerator.java
@@ -18,7 +18,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-    public static final String BASE_URL = "http://10.0.2.2:8080";
+    public static final String BASE_URL = "https://satapp-api.herokuapp.com";
 
     private static HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS);
 
--- satapp/app/src/main/java/com/dmtroncoso/satapp/retrofit/service/SataService.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/retrofit/service/SataService.java
@@ -1,4 +1,12 @@
 package com.dmtroncoso.satapp.retrofit.service;
 
+import com.dmtroncoso.satapp.retrofit.model.UserResponse;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
 public interface SataService {
+
+    @POST("/auth")
+    Call<UserResponse> login();
 }
