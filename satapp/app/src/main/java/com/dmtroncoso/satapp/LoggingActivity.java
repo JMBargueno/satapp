9dbba86fb478f81bf1e3e459aa57f038a5ed7450
myrows <dmtroncoso@AULAS.TRIANA>
2020-02-26 13:32:24 +0100

    Logging form

---
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/LoggingActivity.java
@@ -0,0 +1,26 @@
+package com.dmtroncoso.satapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class LoggingActivity extends AppCompatActivity {
+
+    ConstraintLayout mLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logging);
+
+        mLayout = findViewById(R.id.constraintLayout);
+
+        animationDrawable = (AnimationDrawable) mLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+    }
+}
