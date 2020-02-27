ba2098c81f77a8061aceace5c8f213ff6d676a45
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2020-02-27 10:30:48 +0100

    First commit

---
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/ValidateUserActivity.java
@@ -0,0 +1,14 @@
+package com.dmtroncoso.satapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate_user);
+    }
+}
