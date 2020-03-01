b2a3dca39399a6d72c4cbe1988ce015ced05cece
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-03-01 22:18:59 +0100

    Creada nueva rama para hacer el merge manualmente de la creación del nuevo ticket

--- satapp/app/src/main/java/com/dmtroncoso/satapp/MainActivity.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.dmtroncoso.satapp.common.MyApp;
+import com.dmtroncoso.satapp.tickets.NewTicketActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnScanner;
+    Button btnScanner, btnTicket;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnScanner = findViewById(R.id.buttonScan);
+        btnTicket = findViewById(R.id.buttonTicket);
 
         btnScanner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MyApp.getContext(), QRScannerActivity.class));
+            }
+        });
+
+        btnTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyApp.getContext(), NewTicketActivity.class));
             }
         });
 
