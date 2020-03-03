b06bceec12b1c42fe6544de0e93dda497e0375ae
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2020-03-03 13:54:22 +0100

    Progreso con lista inventoriable

--- satapp/app/src/main/java/com/dmtroncoso/satapp/InventariableListActivity.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/InventariableListActivity.java
@@ -1,14 +1,18 @@
 package com.dmtroncoso.satapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
-public class InventariableListActivity extends AppCompatActivity {
+import com.dmtroncoso.satapp.viewmodel.InventariableViewModel;
 
+public class InventariableListActivity extends AppCompatActivity {
+    InventariableViewModel inventariableViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        inventariableViewModel = new ViewModelProvider(this).get(InventariableViewModel.class);
         setContentView(R.layout.activity_inventariable_list);
     }
 }
