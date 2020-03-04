d0bdc4e114395cea80ccd886e2de7f2deabc2116
Jose Manuel Barguenño López <jmbarguenolopez@gmail.com>
2020-03-04 09:29:08 +0100

    Añadidas constantes al fichero cons

--- satapp/app/src/main/java/com/dmtroncoso/satapp/common/Constants.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/common/Constants.java
@@ -2,4 +2,8 @@ package com.dmtroncoso.satapp.common;
 
 public class Constants {
     public static String UBICACION_DESCONOCIDA = "Ubicación desconocida";
+    public static String TODO_CORRECTO = "Todo correcto";
+    public static String INCIDENCIA_DETECTADA = "Incidencia detectada";
+    public static String RESOLVIENDO_INCIDENCIA = "Resolviendo...";
+
 }
