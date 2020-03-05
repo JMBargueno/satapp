8f1bbf79708f5258147d7c657ab91b25a2bc7da2
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2020-03-05 09:05:12 +0100

    solucionados más conflictos

--- satapp/app/src/main/java/com/dmtroncoso/satapp/inventariables/InventariableListFragment.java
+++ satapp/app/src/main/java/com/dmtroncoso/satapp/inventariables/InventariableListFragment.java
@@ -1,8 +1,12 @@
 package com.dmtroncoso.satapp.inventariables;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,17 +15,29 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.dmtroncoso.satapp.QRScannerActivity;
 import com.dmtroncoso.satapp.R;
+import com.dmtroncoso.satapp.common.MyApp;
+import com.dmtroncoso.satapp.retrofit.generator.ServiceGenerator;
 import com.dmtroncoso.satapp.retrofit.model.InventariableResponse;
 import com.dmtroncoso.satapp.retrofit.model.PagedList;
+import com.dmtroncoso.satapp.retrofit.service.SataService;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class InventariableListFragment extends Fragment {
 
@@ -35,6 +51,9 @@ public class InventariableListFragment extends Fragment {
     private boolean isLastPage = false;
     private boolean isLoading = false;
     private int currentPage;
+    private static final int SCANNER_INVENT_CODE = 2;
+    Context context;
+    SataService service;
 
     public InventariableListFragment() {
     }
@@ -56,6 +75,8 @@ public class InventariableListFragment extends Fragment {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
         inventariableViewModel = new ViewModelProvider(getActivity()).get(InventariableViewModel.class);
+        service = ServiceGenerator.createServiceInventariable(SataService.class);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -102,6 +123,7 @@ public class InventariableListFragment extends Fragment {
         super.onResume();
         currentPage=0;
         loadInventariables(false);
+        Toast.makeText(getActivity(), "onResume()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -132,6 +154,53 @@ public class InventariableListFragment extends Fragment {
                 isLastPage = currentPage == inventariables.getResults().size();
             }
         });
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.barCode:
+                startActivityForResult(new Intent(MyApp.getContext(), QRScannerActivity.class), SCANNER_INVENT_CODE);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SCANNER_INVENT_CODE){
+            if(requestCode == Activity.RESULT_OK){
+                Call<ResponseBody> call = service.getInventariableById(data.getStringExtra("result"));
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if(response.code() == 200){
+                            //Intent intent = new Intent(context, DETAIL.class);
+                            //startActivity(INTENTDETAIL);
+                            Toast.makeText(context, "Permitido.", Toast.LENGTH_SHORT).show();
+                        }else{
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(context, "Error de conexión", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+
     }
 }
 
